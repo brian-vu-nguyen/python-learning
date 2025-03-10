d22b6c546379fee74adf39ffd14eef8c637bf477
Brian Nguyen <brian.vu.nguyen@gmail.com>
2025-03-10 00:24:57 -0700

    saving match_case file

--- src/lessons/20_match_case_statements.py
+++ src/lessons/20_match_case_statements.py
@@ -23,23 +23,13 @@
 # print(day_of_week(7))
 
 
-def day_of_week(day):
+def is_weekend(day):
     match day:
-        case 1:
-            return "It is Sunday"
-        case 2:
-            return "It is Monday"
-        case 3:
-            return "It is Tuesday"
-        case 4:
-            return "It is Wednesday"
-        case 5:
-            return "It is Thursday"
-        case 6:
-            return "It is Friday"
-        case 7:
-            return "It is Saturday"
+        case "Saturday" | "Sunday":
+            return True
+        case "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday":
+            return False
         case _:
             return "Invalid"
 
-print(day_of_week(1))
+print(is_weekend("pizza"))
